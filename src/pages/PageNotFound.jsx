a0325d7019da3e3d66648aa9d5292b99345b0d42
Rally <raul.gheba15@yahoo.ro>
2023-12-17 09:48:48 +0200

    --all

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <p>Page not found 😢</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
